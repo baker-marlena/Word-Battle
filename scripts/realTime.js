1209e7a42812e361b63f2cfa3273e8079938e1c6
Marlena Baker <baker.marlena@gmail.com>
2017-05-17 15:35:38 -0600

    timer now displaying simulatiously for both users, and can be stopped by either user

--- scripts/realTime.js
+++ scripts/realTime.js
@@ -11,18 +11,16 @@ const database = firebase.database();
 var currentSession;
 var userName;
 var otherUser;
+var timeInterval;
 
 // ----- create a new session - working
 $("#sessionKeyGenBut").click(function (){
   let sessionKey = getSessionKey();
   let sessionRef = database.ref(sessionKey);
-  createNewSession(sessionRef);
   currentSession = sessionRef;
   userName = "user_1";
-  setDisabledContent(userName);
-  listenForTyping(userName);
-  //listenForCount(userName);
-  listenForOther(userName);
+  createNewSession(sessionRef);
+  startSession(userName);
   $("#sessionGenSuccessMessage").text("Success! Refresh to create or join a different session.");
 });
 
@@ -54,8 +52,8 @@ function createNewSession (value){
     timer: true,
     timeSet: 0,
     timeLeft: 0,
-    start: false,
-    end: false
+    startStatus: false,
+    initilized:false
   });
 }
 
@@ -64,13 +62,30 @@ $("#sessionKeyAcceptBut").click(function(){
   let keyInput = $("#sessionKeyAccept").val();
   currentSession = database.ref(keyInput);
   userName = "user_2";
-  //if ($("user_1_text").prop("disabled") !== true && $("user_2_text").prop("disabled") !== true) {
-    setDisabledContent(userName);
-//  }
-  listenForTyping(userName);
-  //listenForCount(userName);
-  listenForOther(userName);
+  startSession(userName);
   $("#sessionJoinSuccessMessage").text("Success! Refresh to create or join a different session.");
+})
+
+// ----- initialize session
+function startSession (name){
+  setDisabledContent(name);
+  listenForTyping(name);
+  listenForOther(name);
+  listenToStart();
+  currentSession.update({startStatus:false});
+  currentSession.update({initilized:true});
+}
+
+// ----- Jlisten for start button -- untested
+$("#start").click(function(){
+  let timerInput = $("#timer").val();
+  currentSession.update({startStatus:true,timeSet:timerInput});
+  timerRun();
+});
+
+// ----- listen for end button -- untested
+$("#clearTimer").click(function(){
+  currentSession.update({startStatus:false});
 })
 
 // ----- capture typing -- working
@@ -83,7 +98,6 @@ $(`#${userName}_text`).keyup(function() {
       wordCount: this.value.split(" ").length
     }
   };
-  console.log(update);
   $(`#${userName}_words`).text("Word Count: " +  this.value.split(" ").length);
   currentSession.update(update);
 })
@@ -96,7 +110,7 @@ function listenForOther (name){
     const data = snapshot.val();
     console.log (data.textInput);
     $(`#${otherUser}_text`).val(data.textInput);
-    $(`#${otherUser}_words`).text(data.wordCount);
+    $(`#${otherUser}_words`).text("Word Count: " + data.wordCount);
   })
 }
 
@@ -119,3 +133,85 @@ function setDisabledContent (name){
   $("#sessionKeyGenBut").prop('disabled', true);
   $("#sessionKeyAcceptBut").prop('disabled', true);
   };
+
+  // ----- Database Timer -- untested
+  function timerRun () {
+    let secs;
+    var timerAmmount = currentSession.child('timeSet').once('value').then(function (snapshot){
+      secs = snapshot.val()*60;
+    });
+    console.log(secs);
+    // actual timer intervals
+    timeInterval = setInterval(function(){
+      secs--;
+      console.log("counting:"+secs);
+      currentSession.update({timeLeft:`${calculateMintues(secs)}:${calculateSeconds(secs)}`});
+      // timer runs out
+      if(secs == 0){
+        endTimer();
+        }
+      }, 1000);
+  };
+
+  // ----- listen to database for timer updates and update user's page -- untested
+  function displayTimer () {
+    currentSession.child('timeLeft').on("value", function(snapshot){
+      $("#timer").val(snapshot.val());
+    })
+  }
+
+  // ----- end the timer and update start value -- untested
+  function endTimer (){
+    console.log("Checking timer end:" + timeInterval);
+    currentSession.update({startStatus:false});
+    // ----- set timer field to last entered value
+    let timerValueSet = currentSession.child('timeSet').once('value').then(function(snapshot){
+          $("#timer").val(snapshot.val());
+    })
+    alert ("Time's up!");
+  }
+
+  // ----- watching for the start and stop -- untested
+  function listenToStart (){
+    console.log("Listening for db changes on 'start'");
+    currentSession.child('startStatus').on("value", function(snapshot){
+      let currentValue = snapshot.val();
+      console.log(currentValue);
+      let startButton = $("#start")
+      let endButton = $("#clearTimer")
+      currentSession.child('initilized').once('value').then(function(snapshot){
+        let initStatus = snapshot.val();
+        if (currentValue == true){
+          currentSession.update({timeLeft:0});
+          displayTimer();
+          startButton.prop('disabled', true);
+          endButton.prop('disabled', false);
+        }
+        if (currentValue == false){
+          if (initStatus == true) {
+            clearInterval(timeInterval);
+            endTimer();
+          }
+          currentSession.update({timeLeft:0});
+          currentSession.child('timeLeft').off();
+          startButton.prop('disabled', false);
+          endButton.prop('disabled', true);
+        }
+        console.log("Init check is running",initStatus)
+      })
+    })
+  }
+
+  // calculate remaining minutes -- working
+  function calculateMintues(value){
+    return Math.floor(value/60);
+  }
+
+  //caluclate remaining seconds -- working
+  function calculateSeconds(value){
+    let displaySeconds = value-(Math.floor(value/60)*60);
+    if (displaySeconds < 10) {
+      return `0${displaySeconds}`;
+    }
+    return displaySeconds;
+  }
--- scripts/timer.js
+++ scripts/timer.js
@@ -1,39 +1,24 @@
-// check if timer is active
-function checkTimerStatus (){
-  if ($("#timerOn").checked === true){
-    console.log("Timer On!")
-  }
-  if ($("timerOff".checked === true)) {
-    console.log("Timer off!")
-  }
-}
+// ----- check if timer is active -- not working
+// function checkTimerStatus (){
+//   if ($("#timerOn").checked === true){
+//     console.log("Timer On!")
+//   }
+//   if ($("timerOff".checked === true)) {
+//     console.log("Timer off!")
+//   }
+// }
 
-// runs timer
-function timerRun () {
-  let timerAmmount = $("#timer").val();
-  var secs = timerAmmount*60;
-  var timeInterval = setInterval(function(){
-      secs--; console.log(secs);
-      $("#timer").val(`${calculateMintues(secs)}:${calculateSeconds(secs)}`);
-    if(secs == 0){
-      clearInterval(timeInterval);
-      $("#start").prop("disabled", false);
-      alert ("Time's up!");
-     }
-  }, 1000);
-}
-
-
-// calculate remaining minutes
-function calculateMintues(value){
-  return Math.floor(value/60);
-}
-
-//caluclate remaining seconds
-function calculateSeconds(value){
-  let displaySeconds = value-(Math.floor(value/60)*60);
-  if (displaySeconds < 10) {
-    return `0${displaySeconds}`;
-  }
-  return displaySeconds;
-}
+// ----- runs timer -- working
+// function timerRun () {
+//   let timerAmmount = $("#timer").val();
+//   var secs = timerAmmount*60;
+//   var timeInterval = setInterval(function(){
+//       secs--; console.log(secs);
+//       $("#timer").val(`${calculateMintues(secs)}:${calculateSeconds(secs)}`);
+//     if(secs == 0){
+//       clearInterval(timeInterval);
+//       $("#start").prop("disabled", false);
+//       alert ("Time's up!");
+//      }
+//   }, 1000);
+// }
